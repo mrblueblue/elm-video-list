e48bdf9c4ae2b4dc3b10b3bebbf766b470e65973
Jonathan Huang <grokut@gmail.com>
2015-12-13 18:34:48 -0800

    add video url to href

--- src/Video.elm
+++ src/Video.elm
@@ -1,7 +1,7 @@
 module Video where
 
-import Html exposing (li, text, div, img, h3, p)
-import Html.Attributes exposing (class, src, style)
+import Html exposing (li, text, div, img, h3, p, a)
+import Html.Attributes exposing (class, src, style, href)
 import Json.Encode exposing (string)
 
 type alias Model =
@@ -17,17 +17,19 @@ type alias Model =
   }
 
 view video =
-  li [class "collection-item"] [
-    div [class "thumbnail-container"] [
-      div [
-        style [
-          ("background-image", "url('" ++ video.thumbnail_180_url ++ "')")
-        ]
-      ] []
-    ]
-    , div [class "content"] [
-      h3 [class "title"] [text video.title]
-      , p [class "subtitle"] [text video.screenname]
-      , p [class "description"] [text (toString(video.views_total) ++ " views")]
+  a [href video.url] [
+    li [class "collection-item"] [
+      div [class "thumbnail-container"] [
+        div [
+          style [
+            ("background-image", "url('" ++ video.thumbnail_180_url ++ "')")
+          ]
+        ] []
+      ]
+      , div [class "content"] [
+        h3 [class "title"] [text video.title]
+        , p [class "subtitle"] [text video.screenname]
+        , p [class "description"] [text (toString(video.views_total) ++ " views")]
+      ]
     ]
   ]
